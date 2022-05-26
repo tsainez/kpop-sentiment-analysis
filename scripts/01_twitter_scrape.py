c9b6d7951451dc98f7a7dc67adc4fbe518abcb27
Tony <tsainez@gmail.com>
2022-05-26 15:02:18 +0900

    Update 01_twitter_scrape.py

--- scripts/01_twitter_scrape.py
+++ scripts/01_twitter_scrape.py
@@ -1,20 +1,14 @@
 # For nice readability...
 import sys
 
-# # For saving access tokens and for file management when creating and adding to the dataset
-# import os
-# from genericpath import exists
-
 # For saving the response data in CSV format
 import csv
 
 # For parsing the dates received from twitter in readable formats
-# import datetime
 import dateutil.parser
-# import unicodedata
 
-# # To add wait time between requests
-# import time
+# To add wait time between requests
+import time
 
 # Helper functions that help us scrape Tweets.
 from helpers import *
@@ -34,49 +28,50 @@ if int(max_results) < 10:
     max_results = 10
 
 since_id = None  # For now...
+total = 0 # Tracks the total amount of Tweets retrieved
 
 while(True):
-    url = create_url(keyword, max_results, since_id)
-    json_response = connect_to_endpoint(url[0], headers, url[1])
-
-    # Open OR create the target CSV file
-    path = '../data/data.csv'
-    csvFile = open(path, "a", newline="", encoding='utf-8')
-    csvWriter = csv.writer(csvFile)
-
-    # Loop through each Tweet
     try:
-        for tweet in json_response['data']:
-            # Pull relevant data from the Tweet object
-            created_at = dateutil.parser.parse(tweet['created_at'])
-            id = tweet['id']
-            lang = tweet['lang']
+        url = create_url(keyword, max_results, since_id)
+        json_response = connect_to_endpoint(url[0], headers, url[1])
 
-            # Clean up the Tweet text...
-            text = tweet['text']
-            text = clean(text)
+        # Open OR create the target CSV file
+        path = '../data/data.csv'
+        csvFile = open(path, "a", newline="", encoding='utf-8')
+        csvWriter = csv.writer(csvFile)
 
-            # Assemble all data in a list
-            res = [id, text, created_at, lang]
+        # Loop through each Tweet
+        try:
+            for tweet in json_response['data']:
+                # Pull relevant data from the Tweet object
+                created_at = dateutil.parser.parse(tweet['created_at'])
+                id = tweet['id']
+                lang = tweet['lang']
 
-            # Append the result to the CSV file
-            csvWriter.writerow(res)
+                # Clean up the Tweet text...
+                text = tweet['text']
+                text = clean(text)
 
-        # When done, close the CSV file
-        csvFile.close()
-    except KeyError:
-        sys.exit("\nNo additional Tweets found! Bye.")
+                # Assemble all data in a list
+                res = [id, text, created_at, lang]
 
-    print("\n\tRetrieved " +
-          str(json_response['meta']['result_count']) + " total Tweets.")
+                # Append the result to the CSV file
+                csvWriter.writerow(res)
 
-    # TODO: Run the script every 5 minutes until KeyboardInterrupt
-    print("\nWould you like to continue retrieving Tweets using this keyword? \n\ty/n: ", end='')
-    prompt = input()
+            # When done, close the CSV file
+            csvFile.close()
+        except KeyError:
+            sys.exit("\nNo additional Tweets found! Bye.")
 
-    if prompt != 'y':
-        sys.exit("\nBye.")
+        print("\n\tRetrieved " +
+            str(json_response['meta']['result_count']) + " total Tweets.")
+        total += json_response['meta']['result_count']
 
-    # Else, we only want the most recent Tweets...
-    most_recent = max(json_response['data'], key=lambda ev: ev['id'])
-    since_id = most_recent['id']
+        print("\nWaiting 5 minutes for next request...")
+        time.sleep(300)
+
+        # Else, we only want the most recent Tweets...
+        most_recent = max(json_response['data'], key=lambda ev: ev['id'])
+        since_id = most_recent['id']
+    except KeyboardInterrupt:
+        sys.exit("\n\n\tTotal Tweets retrieved: {}\n\tExiting...\n".format(total))
